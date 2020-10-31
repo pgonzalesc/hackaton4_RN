416780a4ed51a50817b65e376f90c98f641c9be1
pgonzalesc <pedro281195@gmail.com>
2020-10-31 10:50:27 -0500

    Diseño del Formulario

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,8 +6,32 @@ const Login = () => {
         <section className="login">
             <div className="loginContainer">
                 <div className="frameData">
+                    <div className="dataContainer">
+                        <div className="dataTitle">
+                            <h1>Datos de Registro</h1>
+                        </div>
+                        <div className="dataBody">
+                            <p>No hay datos</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="frameForm">
+                    <div className="formHeader">
+                        <label>Registro</label>
+                    </div>
+
+                    <div className="formBody">
+                        <input type="text" placeholder="Nombre" required />
+                        <input type="text" placeholder="Apellido" required />
+                        <input type="text" placeholder="Correo" required />
+                        <input type="text" placeholder="Teléfono" required />
+                        <input type="date" data-placeholder="Fecha de Nacimiento" required aria-required="true"/>
+                        <input type="text" placeholder="Dirección" required />
+                    </div>
+                    
+                    <div className="formFooter">
+                        <button>Registrar</button>
+                    </div>
                 </div>
             </div>
         </section>
