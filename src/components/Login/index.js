e522c65944a9098b4d643cd2aa2139d2016d5aa3
pgonzalesc <pedro281195@gmail.com>
2020-10-31 09:23:43 -0500

    Division del Formulario

---
+++ src/components/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+const Login = () => {
+    return (
+        <section className="login">
+            <div className="loginContainer">
+                <div className="frameData">
+                </div>
+                <div className="frameForm">
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Login;
